c03a74b1584a90a15cd9539c238a994939503362
Joshua Wootonn <joshuawootonn@gmail.com>
2017-07-04 11:30:02 -0500

    Signup complete

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,25 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      flash[:success] = "User created successfully!"
+      redirect_to root_url
+    else
+      flash.now[:danger] = "Invalid email/name!"
+    end
+
+  end
+  def show
+
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email)
+    end
+
+end
